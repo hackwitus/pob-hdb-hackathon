26c55c9535f78cd1d4c21db6bbc4fcf7daffca00
Jackson Schwarz <jackson.schwarz0@gmail.com>
2018-02-05 21:07:16 -0500

    Added the components and services for the inventory and checkout menus

--- pob-client/src/app/app.module.ts
+++ pob-client/src/app/app.module.ts
@@ -1,14 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
 
 import { AppComponent } from './app.component';
+import {InventoryComponent} from "./inventory/inventory.component"
+import {CheckoutComponent} from './checkout/checkout.component';
 
+const appRoutes:Routes=[
+  {path:'', redirectTo:'inventory'},
+  {path:'inventory', component:InventoryComponent},
+  {path:'checkout', component:CheckoutComponent}
+]
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InventoryComponent,
+    CheckoutComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(
+      appRoutes
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
